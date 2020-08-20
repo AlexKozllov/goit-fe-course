9239ea659c247e4a51df14be9f24964d65df4dfa
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2020-08-21 00:54:26 +0300

    added task 8-10

--- javascript/goit-js-hw-06/java-script/task-1.js
+++ javascript/goit-js-hw-06/java-script/task-1.js
@@ -1,8 +1,6 @@
 "use strict";
 import * as user from "./users.js";
-console.log(user);
 const users = user.default;
-console.log(users);
 
 // Задание 1
 // Получить массив имен всех пользователей(поле name).
@@ -86,3 +84,66 @@ const calculateTotalBalance = (users) => {
 };
 
 console.log(calculateTotalBalance(users)); // 20916
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// Задание 8
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => {
+  return users.reduce((acc, el) => {
+    const friend = el.friends.find((elem) => elem === friendName);
+    if (friend !== undefined) {
+      acc.push(el.name);
+    }
+    return acc;
+  }, []);
+};
+
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Задание 9
+// Массив имен(поле name) людей, отсортированных в зависимости от количества их
+// друзей(поле friends)
+
+const getNamesSortedByFriendsCount = (users) => {
+  // return users.reduce((acc, el) => {
+  //   console.log(el.friends.sort((a, b) => a - b));
+  //   return acc;
+  // }, []);
+
+  users.reduce((acc, el) => {
+    el.numbOfFriends = el.friends.length;
+  }, []);
+  users.sort((a, b) => a.numbOfFriends - b.numbOfFriends);
+  return users.reduce((bcc, element) => {
+    bcc.push(element.name);
+    return bcc;
+  }, []);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazque
+
+//////////////////////////////////////////////////////////////////////////
+
+// Задание 10
+// Получить массив всех умений всех пользователей(поле skills), при этом не должно быть
+// повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = (users) => {
+  return users.reduce((acc, elem) => {
+    acc.find((el) => el === elem.skills);
+    acc.push(...elem.skills);
+    acc.sort();
+    return acc.filter((item, pos) => {
+      return acc.indexOf(item) == pos;
+    });
+  }, []);
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'm
