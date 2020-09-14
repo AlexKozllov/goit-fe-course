60b68ad0e2c12f996e3b042720b5bde5ceaeaa27
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2020-09-14 23:09:55 +0300

    correct task-8

--- javascript/goit-js-hw-08/index.js
+++ javascript/goit-js-hw-08/index.js
@@ -2,10 +2,6 @@
 
 import gallery from "./gallery-items.js";
 
-// console.log(gallery);
-
-// Создаем элемент списка галереи
-
 const ulGallery = document.querySelector(".js-gallery");
 const lightboxImage = document.querySelector(".lightbox__image");
 const lightboxButton = document.querySelector(".lightbox__button");
@@ -17,39 +13,25 @@ gallery.forEach((object) => {
   img.setAttribute("src", object.preview);
   img.setAttribute("data-sourse", object.original);
   img.setAttribute("alt", object.description);
-
   const link = document.createElement("a");
   link.classList.add("gallery__link");
-  //   link.setAttribute("href", object.original);
-
-  //   console.log(img);
-  //   console.log(link);
-
   link.append(img);
-  //   console.log(link);
-
   const item = document.createElement("li");
   item.classList.add("gallery__item");
-
   item.append(link);
-  //   console.log(item);
-
-  //   console.log(ulGallery);
   ulGallery.append(item);
-  //   console.log(ulGallery);
 });
 
+function NewGallery(object) {}
+
 ulGallery.addEventListener("click", (event) => {
-  //   console.log(event);
-  console.dir(event.target);
-  //   console.log(event.target.currentSrc);
-  console.log(event.target.dataset.sourse);
   const newSrc = event.target.dataset.sourse;
+
   lightbox.classList.add("is-open");
   lightboxImage.src = newSrc;
   //   window.addEventListener("keydown", (event) => {
   //     console.log(event);
-  //     console.dir(ulGallery);
+  console.dir(ulGallery);
 
   //     if (event.code === "ArrowRight") {
   //       ulGallery.childNodes[1];
