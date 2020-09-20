d8d2c8198740bbdcae75e13638a03ca84dc97b6f
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2020-09-20 23:53:08 +0300

    aaaaaaaa

--- javascript/goit-js-hw-08/index.js
+++ javascript/goit-js-hw-08/index.js
@@ -6,38 +6,65 @@ const ulGallery = document.querySelector(".js-gallery");
 const lightboxImage = document.querySelector(".lightbox__image");
 const lightboxButton = document.querySelector(".lightbox__button");
 const lightbox = document.querySelector(".lightbox");
+const lightboxContent = document.querySelector(".lightbox__content");
 
-gallery.forEach((object) => {
+// gallery.forEach((object) => {
+//   let i = 0;
+//   i = i + 1;
+//   const img = document.createElement("img");
+//   img.classList.add("gallery__image");
+//   img.setAttribute("src", object.preview);
+//   img.setAttribute("data-sourse", object.original);
+//   img.setAttribute("alt", object.description);
+//   img.setAttribute("data-index", i);
+
+//   const link = document.createElement("a");
+//   link.classList.add("gallery__link");
+//   link.append(img);
+//   const item = document.createElement("li");
+//   item.classList.add("gallery__item");
+//   item.append(link);
+//   ulGallery.append(item);
+// });
+
+const arrGallery = gallery.map((object, index) => {
   const img = document.createElement("img");
   img.classList.add("gallery__image");
   img.setAttribute("src", object.preview);
   img.setAttribute("data-sourse", object.original);
   img.setAttribute("alt", object.description);
+  img.setAttribute("data-index", index + 1);
+
   const link = document.createElement("a");
   link.classList.add("gallery__link");
   link.append(img);
   const item = document.createElement("li");
   item.classList.add("gallery__item");
   item.append(link);
-  ulGallery.append(item);
+  return item;
+  // ulGallery.append(item);
+  // return ulGallery;
 });
-
-function NewGallery(object) {}
+ulGallery.append(...arrGallery);
 
 ulGallery.addEventListener("click", (event) => {
   const newSrc = event.target.dataset.sourse;
-
+  // const newSrc = event.target.dataset.indexNumber ;
+  // console.log(event.target.dataset);
   lightbox.classList.add("is-open");
   lightboxImage.src = newSrc;
   //   window.addEventListener("keydown", (event) => {
   //     console.log(event);
-  console.dir(ulGallery);
+  // console.dir(ulGallery);
 
-  //     if (event.code === "ArrowRight") {
-  //       ulGallery.childNodes[1];
-  //       // console.log(ulGallery.childNodes[i]);
-  //     }
-  //   });
+  body.addEventListener("keydown", (event) => {
+    console.log(5);
+    console.log(event.target.dataset);
+    // arrGallery.forEach(el => {
+    // if (){}
+    // }
+    // )
+  });
 });
 
 lightboxButton.addEventListener("click", () => {
