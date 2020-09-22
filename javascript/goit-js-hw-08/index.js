5bcb91f106603bc952ff2ed5900a8a946856e8c1
AlexKozzlov <alexandr.kozlov1120@gmail.com>
2020-09-22 16:41:04 +0300

    optimized code

--- javascript/goit-js-hw-08/index.js
+++ javascript/goit-js-hw-08/index.js
@@ -8,25 +8,6 @@ const lightboxButton = document.querySelector(".lightbox__button");
 const lightbox = document.querySelector(".lightbox");
 const lightboxContent = document.querySelector("lightbox__overlay");
 
-// gallery.forEach((object) => {
-//   let i = 0;
-//   i = i + 1;
-//   const img = document.createElement("img");
-//   img.classList.add("gallery__image");
-//   img.setAttribute("src", object.preview);
-//   img.setAttribute("data-sourse", object.original);
-//   img.setAttribute("alt", object.description);
-//   img.setAttribute("data-index", i);
-
-//   const link = document.createElement("a");
-//   link.classList.add("gallery__link");
-//   link.append(img);
-//   const item = document.createElement("li");
-//   item.classList.add("gallery__item");
-//   item.append(link);
-//   ulGallery.append(item);
-// });
-
 const arrGallery = gallery.map((object, index) => {
   const img = document.createElement("img");
   img.classList.add("gallery__image");
@@ -42,60 +23,59 @@ const arrGallery = gallery.map((object, index) => {
   item.classList.add("gallery__item");
   item.append(link);
   return item;
-  // ulGallery.append(item);
-  // return ulGallery;
 });
 ulGallery.append(...arrGallery);
 
-const galleryClick = (event) => {
-  let newIndex = event.target.dataset.index;
-  // let newIndex = event.target.attributes[4].value;
-  // let newIndex = 1;
-  lightboxImage.src = gallery[newIndex - 1].original;
+let newIndex;
 
-  lightbox.classList.add("is-open");
-
-  // const newSrc = event.target.dataset.indexNumber;
-  function hendleIndex(ev) {
-    if (ev.code === "ArrowRight") {
-      newIndex++;
-    }
-    if (ev.code === "ArrowLeft") {
-      newIndex--;
-    }
-    if (newIndex > ulGallery.childElementCount) {
-      newIndex = 1;
-    }
-    if (newIndex < 1) {
-      newIndex = ulGallery.childElementCount;
-    }
-    console.log(newIndex);
-    lightboxImage.src = gallery[newIndex - 1].original;
+const hendleIndex = (ev) => {
+  if (ev.code === "ArrowRight") {
+    newIndex++;
   }
-  window.addEventListener("keydown", hendleIndex);
-  window.addEventListener("keydown", escape);
-  // window.removeEventListener("keydown", hendleIndex);
+  if (ev.code === "ArrowLeft") {
+    newIndex--;
+  }
+  if (newIndex > ulGallery.childElementCount) {
+    newIndex = 1;
+  }
+  if (newIndex < 1) {
+    newIndex = ulGallery.childElementCount;
+  }
+  lightboxImage.src = gallery[newIndex - 1].original;
 };
-ulGallery.addEventListener("click", galleryClick);
+
+const hendleEscape = (event) => {
+  if (event.code === "Escape") {
+    lightbox.classList.remove("is-open");
+    lightboxImage.src = "";
+    window.removeEventListener("keydown", hendleEscape);
+    window.removeEventListener("keydown", hendleIndex);
+  }
+};
+
+const hendleGalleryClick = (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.classList.add("is-open");
+  newIndex = event.target.dataset.index;
+  lightboxImage.src = gallery[newIndex - 1].original;
+  window.addEventListener("keydown", hendleIndex);
+  window.addEventListener("keydown", hendleEscape);
+};
+
+ulGallery.addEventListener("click", hendleGalleryClick);
 
 lightboxButton.addEventListener("click", () => {
   lightbox.classList.remove("is-open");
   lightboxImage.src = "";
-  // window.removeEventListener("keydown", hendleIndex);
+  window.removeEventListener("keydown", hendleEscape);
+  window.removeEventListener("keydown", hendleIndex);
 });
+
 lightbox.addEventListener("click", () => {
   lightbox.classList.remove("is-open");
   lightboxImage.src = "";
-  // window.removeEventListener("keydown", hendleIndex);
+  window.removeEventListener("keydown", hendleEscape);
+  window.removeEventListener("keydown", hendleIndex);
 });
-
-const escape = (event) => {
-  console.log(event);
-  if (event.code === "Escape") {
-    // window.removeEventListener("keydown", hendleIndex);
-
-    lightbox.classList.remove("is-open");
-    lightboxImage.src = "";
-    window.removeEventListener("keydown", escape);
-  }
-};
